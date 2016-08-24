2de991ff0cf72a69ab8c6d6294f353dcde1f5d04
arthureggert <opa.roa@gmail.com>
2016-08-24 08:23:51 -0300

    Update custom.aliases.bash
    add git reset commmand

--- aliases/custom.aliases.bash
+++ aliases/custom.aliases.bash
@@ -1,3 +1,8 @@
 alias ls='ls --color=auto -lai'
+
+#ARCH
 alias systemupdate='sudo pacman -Syyu && yaourt -Syu --aur --noconfirm'
 alias pacman='sudo pacman'
+
+#GIT
+alias greset='git reset --hard origin\dev'
