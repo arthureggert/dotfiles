889c1d6ed30967fd69782462a1e5d0cb317d21df
Arthur Henrique Eggert <opa.roa@gmail.com>
2016-08-27 14:16:56 -0300

    ajustes paa enables do bash-it na instação

--- install.sh
+++ install.sh
@@ -12,3 +12,7 @@ ln -s ~/Documentos/workspace/dotfiles/npm/npmrc ~/.npmrc
 ln -s ~/Documentos/workspace/dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/Documentos/workspace/dotfiles/git/gitignore ~/.gitignore
 
+#BASH-IT
+bash-it enable alias atom clipboard curl git maven npm tmux vim
+bash-it enable plugin battery dirs extract git java javascript node proxy ssh tmux
+bash-it enable completion defaults dirs git gradle grunt gulp jboss7 maven npm ssh tmux
