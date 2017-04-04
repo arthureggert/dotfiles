9c66730bbc66951ccd850cbdea37b93302f091aa
arthur.eggert <arthur.eggert@senior.com.br>
2017-04-04 13:04:09 -0300

    fixed issues

--- install.bat
+++ install.bat
@@ -1,2 +1,3 @@
 mklink %HOMEPATH%\.bashrc %HOMEPATH%\Documents\Workspace\dotfiles\bash\bashrc
 mklink %HOMEPATH%\.bash_profile %HOMEPATH%\Documents\Workspace\dotfiles\bash\bash_profile
+mklink %HOMEPATH%\.gitignore %HOMEPATH%\Documents\Workspace\dotfiles\git\gitignore
