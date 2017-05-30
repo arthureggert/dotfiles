d0b5bdb8c36c786e1c4526aca0496006f923027e
Arthur H. Eggert <arthur.eggert@senior.com.br>
2017-05-30 13:53:36 -0300

    batatinha

--- install.bat
+++ install.bat
@@ -1,3 +1,4 @@
 mklink %HOMEPATH%\.bashrc %HOMEPATH%\Documents\Workspace\dotfiles\bash\bashrc
 mklink %HOMEPATH%\.bash_profile %HOMEPATH%\Documents\Workspace\dotfiles\bash\bash_profile
 mklink %HOMEPATH%\.gitignore %HOMEPATH%\Documents\Workspace\dotfiles\git\gitignore
+REM mklink c:\Users\arthur.eggert\.babun\cygwin\home\arthur.eggert\.zshrc c:\Users\arthur.eggert\Documents\Workspace\dotfiles\zsh\zshrc
