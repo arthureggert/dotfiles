f659e182dd8f14236df6eb760dd3beb9d58b7221
arthur.eggert <arthur.eggert@senior.com.br>
2017-10-30 11:27:23 -0200

    update

--- bash/aliases/work.alias.bash
+++ bash/aliases/work.alias.bash
@@ -1,1 +1,5 @@
 alias open_tasy='javaws ~/Desktop/tasy-acesso-interno.jnlp'
+alias run_ecm='cd ~/Documents/Workspace/ecm-frontend/ && npm start'
+alias run_cockpit='cd ~/Documents/Workspace/cockpit-frontend/ && npm start'
+alias run_simple='cd ~/Documents/Workspace/simplified_process_editor/ && npm start'
+alias run_modelador='cd ~/Documents/Workspace/modelador-frontend/ && npm run build:dev && npm start'
