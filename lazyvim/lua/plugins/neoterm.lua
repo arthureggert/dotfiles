2974308e9c0df3a8f24b5965208a4c856982b361
Arthur Eggert <opa.roa@gmail.com>
2023-02-13 20:49:12 +0100

    neoterm keymaps

--- lazyvim/lua/plugins/neoterm.lua
+++ lazyvim/lua/plugins/neoterm.lua
@@ -1,7 +1,8 @@
 return {
   "kassio/neoterm",
   config = function()
-    vim.g.neoterm_autoinsert = 1
-    vim.g.neoterm_default_mod = "botright"
+    vim.g.neoterm_autoinsert = 0
+    vim.g.neoterm_default_mod = "belowright"
+    vim.g.neoterm_size = 30
   end,
 }
