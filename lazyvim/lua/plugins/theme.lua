50fa2c36216da865b696bced34c44060212f5278
Arthur Eggert <opa.roa@gmail.com>
2023-04-04 15:50:45 +0200

    2023.04.04 @ 15:50

--- lazyvim/lua/plugins/theme.lua
+++ lazyvim/lua/plugins/theme.lua
@@ -1,9 +1,24 @@
 return {
   { "Mofiqul/dracula.nvim" },
+  -- {
+  -- "catppuccin/nvim",
+  -- lazy = true,
+  -- name = "catppuccin",
+  -- opts = {
+  -- flavour = "frappe", -- latte, frappe, macchiato, mocha
+  -- },
+  -- },
+  -- {
+  -- "folke/tokyonight.nvim",
+  -- lazy = true,
+  -- opts = { style = "storm" },
+  -- },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "dracula",
+      -- colorscheme = "catppuccin",
+      -- colorscheme = "tokyonight",
     },
   },
 }
