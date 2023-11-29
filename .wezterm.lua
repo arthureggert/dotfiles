71e9ac165edcc12135e11f1efbdb223dd65f7c58
Arthur Eggert <opa.roa@gmail.com>
2023-11-29 16:10:24 +0100

    2023.11.29 @ 16:10

--- .wezterm.lua
+++ .wezterm.lua
@@ -23,7 +23,7 @@ config.window_background_opacity = 1
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = true
 
-config.font = wezterm.font("JetBrainsMono Nerd Font")
+-- config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 22.0
 
 config.keys = {
