afc1987a17085f147a574d4e791a1659b7348b33
Arthur Eggert <opa.roa@gmail.com>
2024-03-19 21:06:19 +0100

    2024.03.19 @ 21:06

--- scripts/alias.zsh
+++ scripts/alias.zsh
@@ -1,13 +1,3 @@
-alias google='web_search google'
-alias ddg='web_search duckduckgo'
-alias github='web_search github'
-
-
-# bangs
-alias wiki='web_search duckduckgo \!w'
-alias youtube='web_search duckduckgo \!yt'
-alias map='web_search duckduckgo \!m'
-
 alias c='z'
 alias vim='nvim'
 alias vi='vim'
--- scripts/commands.zsh
+++ scripts/commands.zsh
@@ -1,48 +1,5 @@
 #!/usr/bin/env zsh
 
-function todo() {
-  if [[ "$1" == '--work' ]]; then
-    pter $HOME/Documents/workbech/todo/work.txt
-  else 
-    pter $HOME/Documents/workbech/todo/todo.txt
-  fi
-}
-
-function idea() {
-  IDEA = "IntelliJ IDEA Ultimate"
-  # Check current working directory
-  wd=""
-  if [ -z "$1" ]; then
-	  wd=$(pwd)
-  elif [ -d "$1" ]; then
-	  wd=$(ls -1d "$1" 2>&1 | head -n1)
-  fi
-
-  # Check if we were given a file
-  if [ -f "$1" ]; then
-	  open -a "$IDEA" "$1"
-  else
-	  # Check working directory
-  	pushd $wd > /dev/null
-
-	  if [ -d ".idea" ]; then
-		  # Handle .idea folders
-		  open -a "$IDEA" .
-	  elif [ -f *.ipr ]; then
-		  # Handle idea project files
-		  open -a "$IDEA" `ls -1d *.ipr | head -n1`
-	  elif [ -f pom.xml ]; then
-		  # Handle pom.xml
-		  open -a "$IDEA" "pom.xml"
-	  else
-		# Can't do anything else, just open Intellij
-		open "$IDEA"
-	fi
-
-	popd > /dev/null
-fi
-}
-
 function create_docker_separator() {
   defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}'; killall Dock
 }
