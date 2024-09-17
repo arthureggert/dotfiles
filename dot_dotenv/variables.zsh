868bef6a60f2d31c73159372f8f13091e4d991fd
Arthur Eggert <opa.roa@gmail.com>
2024-09-17 09:37:29 +0200

    add new gh token

--- dot_dotenv/variables.zsh
+++ dot_dotenv/variables.zsh
@@ -1,1 +1,3 @@
 export COMPANY_USER_NAME=$(op read "op://env/zenjob/username")
+export IDENTITYTM_GITLAB_ACCESS_TOKEN=$(op read "op://env/zenjob/gitlab-access-token")
+export GITHUB_NPM_TOKEN=$(op read "op://env/zenjob/gh-access-token")
