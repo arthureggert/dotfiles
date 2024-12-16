2fb8b7ef0ea14d53b5e7b238c886b302971a393f
Arthur Eggert <opa.roa@gmail.com>
2024-12-16 19:06:38 +0100

    python home work across machines

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,6 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local homedir = os.getenv("HOME")
+
 vim.opt.winbar = "%=%m %f"
 vim.opt.spelllang = { "en", "pt_br" }
 
@@ -8,8 +10,7 @@ vim.g.mapleader = ","
 vim.g.maplocalleader = ","
 
 vim.g.vim_markdown_folding_disabled = 1
-
-vim.g.python3_host_prog = "/Users/arthur.eggert/.virtualenvs/nvim/bin/python"
+vim.g.python3_host_prog = homedir .. "/.virtualenvs/nvim/bin/python"
 
 vim.wo.wrap = true
 vim.wo.linebreak = true
