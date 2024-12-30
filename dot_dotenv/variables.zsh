33e7c1aa653af0dd5505bbea19882a6794050eb2
Arthur Eggert <opa.roa@gmail.com>
2024-12-30 13:09:54 +0100

    add tmux and ghostty

--- dot_dotenv/variables.zsh
+++ dot_dotenv/variables.zsh
@@ -1,3 +1,4 @@
 export COMPANY_USER_NAME=$(op read "op://env/zenjob/username")
 export IDENTITYTM_GITLAB_ACCESS_TOKEN=$(op read "op://env/zenjob/gitlab-access-token")
 export GITHUB_NPM_TOKEN=$(op read "op://env/zenjob/gh-access-token")
+export OPEN_WEATHER_API_KEY=$(op read "op://env/zenjob/openweather-api")
