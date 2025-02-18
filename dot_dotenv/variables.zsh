19b2123f25160e6be7477525a469bb0bf0711555
Arthur Eggert <opa.roa@gmail.com>
2025-02-18 08:56:47 +0100

    remove 1pasword items

--- dot_dotenv/variables.zsh
+++ dot_dotenv/variables.zsh
@@ -1,4 +1,2 @@
-export COMPANY_USER_NAME=$(op read "op://env/zenjob/username")
-export IDENTITYTM_GITLAB_ACCESS_TOKEN=$(op read "op://env/zenjob/gitlab-access-token")
-export GITHUB_NPM_TOKEN=$(op read "op://env/zenjob/gh-access-token")
-export OPEN_WEATHER_API_KEY=$(op read "op://env/zenjob/openweather-api")
+# export IDENTITYTM_GITLAB_ACCESS_TOKEN=$(op read "op://env/zenjob/gitlab-access-token")
+# export GITHUB_NPM_TOKEN=$(op read "op://env/zenjob/gh-access-token")
