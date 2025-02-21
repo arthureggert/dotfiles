197972cbab8b590239ad527f7d3a27ceb600197a
Arthur Eggert <opa.roa@gmail.com>
2025-02-21 13:36:54 +0100

    new font

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -195,6 +195,7 @@ config.disable_default_key_bindings = true
 -- config.send_composed_key_when_left_alt_is_pressed = true
 -- config.send_composed_key_when_right_alt_is_pressed = true
 config.font_size = 20.0
+config.font = wezterm.font("Hack Nerd Font")
 
 config.window_decorations = "TITLE | RESIZE"
 config.window_background_opacity = 1
