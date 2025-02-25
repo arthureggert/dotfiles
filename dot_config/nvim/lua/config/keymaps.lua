4ad04310985c54809acac29b0a986d71b9597622
Arthur Eggert <opa.roa@gmail.com>
2025-02-25 12:41:06 +0100

    feat(keymaps): add silent option for shift arrow key mappings

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,16 +1,29 @@
--- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Keymaps are automatically loaded on the VeryLazy event
 -- Add any additional keymaps here
-vim.keymap.set("n", "<S-Up>", ":m .-2<CR>==", {
+
+vim.keymap.set("x", "<S-Up>", ":m-2<CR>gv=gv", {
   noremap = true,
+  silent = true,
 })
 
-vim.keymap.set("n", "<S-Down>", ":m .+1<CR>==", {
+vim.keymap.set("x", "<S-Down>", ":m'>+<CR>gv=gv", {
   noremap = true,
+  silent = true,
 })
 
-vim.keymap.set("n", "<S-Left>", ":bp<CR>")
-vim.keymap.set("n", "<S-Right>", ":bn<CR>")
+vim.keymap.set("n", "<S-Up>", ":<C-u>move-2<CR>==", {
+  noremap = true,
+  silent = true,
+})
+
+vim.keymap.set("n", "<S-Down>", ":<C-u>move+<CR>==", {
+  noremap = true,
+  silent = true,
+})
+
+vim.keymap.set("n", "<S-Left>", ":bp<CR>", { silent = true })
+vim.keymap.set("n", "<S-Right>", ":bn<CR>", { silent = true })
 
 vim.keymap.set("i", "<C-g>", "<c-g>u<Esc>[s1z=`]a<c-g>u")
 vim.keymap.set("n", "<C-g>", "[s1z=<c-o>")
@@ -19,8 +32,11 @@ vim.keymap.set("n", "-", require("oil").open, {
   desc = "Open parent directory",
 })
 
-vim.keymap.set({ "i", "x", "n", "s" }, "<D-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
-vim.keymap.set({ "i", "x", "n", "s" }, "<D-w>", "<cmd>:bd<cr><esc>", { desc = "Close Buffer" })
-vim.keymap.set({ "i", "x", "n", "s" }, "<D-q>", "<cmd>qa<cr><esc>", { desc = "Close Neovide" })
-vim.keymap.set({ "n" }, "<D-v>", '"+p', { desc = "Paste" })
-vim.keymap.set({ "i" }, "<D-v>", '<esc>"+p<esc>A', { desc = "Paste" })
+vim.keymap.set("n", "<D-s>", "<cmd>w<cr><esc>", { desc = "Save file", silent = true })
+vim.keymap.set("i", "<D-s>", "<esc><cmd>w<cr><esc>a", { desc = "Save file", silent = true })
+
+vim.keymap.set("n", "<D-w>", "<cmd>:bd<cr><esc>", { desc = "Close Buffer" })
+vim.keymap.set("n", "<D-q>", "<cmd>qa<cr><esc>", { desc = "Close Neovide" })
+
+vim.keymap.set("i", "<D-v>", '<esc>"+p<esc>A', { desc = "Paste" })
+vim.keymap.set("n", "<D-v>", '"+p', { desc = "Paste" })
