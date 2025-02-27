3189463ef3817e704b10fefaf9553d48f83c0132
Arthur Eggert <opa.roa@gmail.com>
2025-02-27 13:37:09 +0100

    feat(wezterm): add quick URL opener with CMD+o shortcut

--- dot_config/wezterm/mappings.lua
+++ dot_config/wezterm/mappings.lua
@@ -4,6 +4,15 @@ local neovim = require("neovim")
 -- To send a command to TMUX it needs to be prefixed with \x02
 -- To send a command to NVIM it needs to be prefixed with \x1b
 
+local open_link_action = wezterm.action.QuickSelectArgs({
+	label = "open url",
+	patterns = { "https?://\\S+" },
+	action = wezterm.action_callback(function(window, pane)
+		local url = window:get_selection_text_for_pane(pane)
+		wezterm.open_with(url)
+	end),
+})
+
 local M = {}
 
 local keys = {
@@ -13,12 +22,13 @@ local keys = {
 	{ key = "t", mods = "CMD", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
 	{ key = "e", mods = "CMD", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 	{ key = "e", mods = "CMD|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	{ mods = "CMD", key = "LeftArrow", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "LeftArrow", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Left") },
 	{ key = "RightArrow", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Right") },
 	{ key = "UpArrow", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Up") },
 	{ key = "DownArrow", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Down") },
 	{ key = "v", mods = "CMD", action = wezterm.action({ PasteFrom = "Clipboard" }) },
 	{ key = "c", mods = "CMD", action = wezterm.action({ CopyTo = "Clipboard" }) },
+	{ key = "o", mods = "CMD", action = open_link_action },
 	neovim.bind("w", { SendString = "\x1b:bd\n" }, wezterm.action.CloseCurrentPane({ confirm = true })),
 	neovim.bind("q", { SendString = "\x1b:qa\n" }, wezterm.action.CloseCurrentTab({ confirm = true })),
 	neovim.bind("s", { SendString = "\x1b:w\n" }),
