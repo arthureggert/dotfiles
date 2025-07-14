1451746754ab81e70f1d29eeabd00758f307310c
Arthur Eggert <opa.roa@gmail.com>
2025-07-14 08:47:20 +0200

    feat(nvim): incorporate Comment.nvim plugin for enhanced commenting

--- dot_config/nvim/lua/plugins/theme.lua
+++ dot_config/nvim/lua/plugins/theme.lua
@@ -1,12 +1,15 @@
 return {
-  "binhtran432k/dracula.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
+  -- "binhtran432k/dracula.nvim",
+  --lazy = false,
+  --priority = 1000,
+  --opts = {},
+  { "dracula-pro/vim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "dracula",
+      colorscheme = function()
+        vim.cmd("colorscheme dracula_pro_van_helsing")
+      end,
     },
   },
 }
