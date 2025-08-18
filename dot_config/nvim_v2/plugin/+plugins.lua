42624e3250c0a3b748c6cb13efbc5ad69bafccc3
Arthur Eggert <opa.roa@gmail.com>
2025-08-18 16:06:45 +0200

    feat: neovim 0.12 setup

--- dot_config/nvim_v2/plugin/+plugins.lua
+++ dot_config/nvim_v2/plugin/+plugins.lua
@@ -1,8 +1,134 @@
--- ~/.config/nvim-new/plugin/plugins.lua
 vim.pack.add({
-    { src = "https://github.com/lewis6991/gitsigns.nvim" },
     { src = "https://github.com/mason-org/mason.nvim" },
+    { src = "https://github.com/dracula-pro/vim" },
+    { src = "https://github.com/folke/snacks.nvim" },
+    {
+        src = "https://github.com/saghen/blink.cmp",
+        version = vim.version.range("^1")
+    },
 }, { load = true })
 
-require('gitsigns').setup({ signcolumn = false })
-require("mason").setup({ })
+vim.env.PATH = vim.fn.stdpath("data") .. "/mason/bin:" .. vim.env.PATH
+
+require("mason").setup({})
+
+require('blink.cmp').setup({
+    fuzzy = { implementation = 'prefer_rust_with_warning' },
+    signature = { enabled = true },
+    keymap = {
+        preset = "default",
+        ["<C-space>"] = {},
+        ["<C-p>"] = {},
+        ["<Tab>"] = {},
+        ["<S-Tab>"] = {},
+        ["<C-y>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-n>"] = { "select_and_accept" },
+        ["<C-k>"] = { "select_prev", "fallback" },
+        ["<C-j>"] = { "select_next", "fallback" },
+        ["<C-b>"] = { "scroll_documentation_down", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-l>"] = { "snippet_forward", "fallback" },
+        ["<C-h>"] = { "snippet_backward", "fallback" },
+        -- ["<C-e>"] = { "hide" },
+    },
+
+    appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "normal",
+    },
+
+    completion = {
+        documentation = {
+            auto_show = true,
+            auto_show_delay_ms = 200,
+        }
+    },
+
+    cmdline = {
+        keymap = {
+            preset = 'inherit',
+            ['<CR>'] = { 'accept_and_enter', 'fallback' },
+        },
+    },
+
+    sources = { default = { "lsp" } }
+})
+
+require("snacks").setup({
+    animate = { enabled = false },
+    bigfile = { enabled = false },
+    bufdelete = { enabled = true },
+    dashboard = { enabled = false },
+    debug = { enabled = false },
+    dim = { enabled = false },
+    explorer = {
+        enabled = true,
+        replace_netrw = true,
+        finder = "explorer",
+        sort = { fields = { "sort" } },
+        supports_live = true,
+        tree = true,
+        watch = true,
+        diagnostics = true,
+        diagnostics_open = false,
+        git_status = true,
+        git_status_open = false,
+        git_untracked = true,
+        follow_file = true,
+        focus = "list",
+        auto_close = false,
+        jump = { close = false },
+        layout = { preset = "sidebar", preview = false },
+        formatters = {
+            file = { filename_only = true },
+            severity = { pos = "right" },
+        },
+        matcher = { sort_empty = false, fuzzy = false },
+        config = function(opts)
+            return require("snacks.picker.source.explorer").setup(opts)
+        end,
+        win = {
+            list = {
+                keys = {
+                    ["<BS>"] = "explorer_up",
+                    ["a"] = "explorer_add",
+                    ["d"] = "explorer_del",
+                    ["r"] = "explorer_rename",
+                    ["P"] = "toggle_preview",
+                    ["I"] = "toggle_ignored",
+                    ["H"] = "toggle_hidden",
+                },
+            },
+        },
+    },
+    git = { enabled = false },
+    gitbrowse = { enabled = false },
+    image = { enabled = false },
+    indent = { enabled = false },
+    input = { enabled = false },
+    layout = { enabled = false },
+    lazygit = { enabled = false },
+    notifier = { enabled = false },
+    notify = { enabled = false },
+    picker = {
+        enabled = true,
+        hidden = true,
+        ignored = true,
+        sources = {
+            files = { ignored = true, hidden = true },
+        }
+    },
+    profiler = { enabled = false },
+    quickfile = { enabled = false },
+    rename = { enabled = false },
+    scope = { enabled = false },
+    scratch = { enabled = false },
+    scroll = { enabled = false },
+    statuscolumn = { enabled = false },
+    terminal = { enabled = false },
+    toggle = { enabled = false },
+    util = { enabled = false },
+    win = { enabled = false },
+    words = { enabled = false },
+    zen = { enabled = false },
+})
--- dot_config/nvim_v2/plugin/options.lua
+++ dot_config/nvim_v2/plugin/options.lua
@@ -4,15 +4,15 @@ local opt = vim.opt
 local homedir = os.getenv("HOME")
 
 opt.guicursor = {
-   'n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
-  'i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
-  'r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100'
+    'n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
+    'i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
+    'r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100'
 }
-opt.colorcolumn = "120" 
-opt.signcolumn = "yes:1" 
-opt.termguicolors = true 
+opt.colorcolumn = "120"
+opt.signcolumn = "yes:1"
+opt.termguicolors = true
 opt.ignorecase = true
-opt.swapfile = false 
+opt.swapfile = false
 opt.autoindent = true
 opt.expandtab = true
 opt.tabstop = 4
@@ -24,16 +24,20 @@ opt.list = true
 opt.number = true
 opt.relativenumber = true
 opt.statuscolumn = "%@SignCb@%s%=%T%@NumCb@%l %r│%T"
-opt.wrap = false 
+opt.wrap = false
 opt.cursorline = true
-opt.scrolloff = 8 
-opt.inccommand = "nosplit" 
+opt.scrolloff = 8
+opt.inccommand = "nosplit"
 opt.undodir = homedir .. "/.vim/undodir"
 opt.undofile = true
 opt.completeopt = { "menuone", "popup", "noinsert" }
 opt.winborder = "rounded"
 opt.hlsearch = false
-opt.winbar = "%=%m %f" 
-
+opt.winbar = "%=%m %f"
 
 vim.cmd.filetype("plugin indent on")
+
+vim.cmd.colorscheme("dracula_pro_van_helsing")
+
+vim.g.netrw_liststyle = 1
+vim.g.netrw_sort_by = "size"
