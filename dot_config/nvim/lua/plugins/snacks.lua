4a2f00f8e7beffdfd5a1b5199b1382d1361d860c
Arthur Eggert <opa.roa@gmail.com>
2025-08-22 18:06:33 +0200

    feat: allow hidden and ignored files to be displaues as source for snacks

---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        hidden = true,
+        ignored = true,
+        sources = {
+          files = { ignored = true, hidden = true },
+        },
+        win = {
+          input = {
+            keys = {},
+          },
+        },
+      },
+    },
+  },
+}
