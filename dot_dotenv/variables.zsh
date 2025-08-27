c36a85b87dbba02375713b97fffe5cedc2c024a1
Arthur Eggert <opa.roa@gmail.com>
2025-08-27 12:35:24 +0200

    feat: adjust

--- dot_dotenv/variables.zsh
+++ dot_dotenv/variables.zsh
@@ -1,5 +1,5 @@
-export DEVOPS_GITHUB_PACKAGES_TOKEN=$(gh auth token)
 export HOMEBREW_GITHUB_API_TOKEN=$(gh auth token)
+export DEVOPS_GITHUB_PACKAGES_TOKEN=$(gh auth token)
 export export EZA_COLORS="\
 uu=36:\
 uR=31:\
