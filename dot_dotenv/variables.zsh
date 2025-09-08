9742d12db482d7e74334db0c14a8f0c9891a8a5b
Arthur Eggert <opa.roa@gmail.com>
2025-09-08 18:26:57 +0200

    feat: gh tpoken

--- dot_dotenv/variables.zsh
+++ dot_dotenv/variables.zsh
@@ -1,6 +1,7 @@
 export HOMEBREW_GITHUB_API_TOKEN=$(gh auth token)
 export DEVOPS_GITHUB_PACKAGES_TOKEN=$(gh auth token)
 export GITHUB_NPM_TOKEN=$(gh auth token)
+export GH_TOKEN=$(gh auth token)
 
 export DOCKER_HOST=unix://${HOME}/.colima/default/docker.sock
 export TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE=/var/run/docker.sock
